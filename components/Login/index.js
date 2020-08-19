a2e3b419dc231be07222444309d97e2da1a06d9a
bangdong.chen <bangdong.chen@crgrcent.com>
2020-08-19 18:11:19 +0800

    feat:  新特性

--- components/Login/index.js
+++ components/Login/index.js
@@ -4,6 +4,7 @@ import AV from "leancloud-storage";
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
+import { Form, Input, Button, Checkbox } from 'antd';
 
 import styles from "./index.module.scss";
 
