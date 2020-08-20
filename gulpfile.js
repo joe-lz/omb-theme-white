3276907891ee68c04467eeaff1a9ad021624752e
bangdong.chen <bangdong.chen@crgrcent.com>
2020-08-20 18:05:06 +0800

    fixed:  bug修复

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const gulp = require("gulp");
 function copyComponents() {
   return gulp
     .src(["components/**/**"])
-    .pipe(gulp.dest("../leanblog/src/components/www"));
+    .pipe(gulp.dest("../oh-my-blog/src/components/www"));
 }
 
 function copyPages() {
   return gulp
     .src(["pages/**/**"])
-    .pipe(gulp.dest("../leanblog/src/pages/www"));
+    .pipe(gulp.dest("../oh-my-blog/src/pages/www"));
 }
 
 exports.watch = function () {
